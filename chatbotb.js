93f1de4b7c83d8cdd655f23b662e2c1ce4bc45bf
B11208054 <jin75678738@gmail.com>
2026-05-06 12:22:28 +0800

    Update chatbotb.js

--- chatbotb.js
+++ chatbotb.js
@@ -3,7 +3,7 @@ class ChatBot {
     constructor() {
         // ⚠️ Important: Replace with your n8n webhook URL
         // ****************************************************************
-        this.webhookUrl = 'https://https://b11208054.app.n8n.cloud/webhook/chatb';
+        this.webhookUrl = 'https://b11208054.app.n8n.cloud/webhook/chatb';
         // ****************************************************************        
         // Initialize session ID
         this.sessionId = this.getOrCreateSessionId();
